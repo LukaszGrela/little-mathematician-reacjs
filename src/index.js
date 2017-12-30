58bb0dfc6c247491468eff2ccfdc0a7ac1f139db
Lukasz <lukasz.grela@gmail.com>
2017-12-30 23:00:32 +0000

    relative path build

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<Router basename='/little-mathematician'><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
