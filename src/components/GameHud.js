6211cb93319dbb5e3dbc1ba36a8d98e87b2a6b7e
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-03-02 12:22:06 +0000

    Removed unused state, fixed even/odd class.

--- src/components/GameHud.js
+++ src/components/GameHud.js
@@ -21,16 +21,12 @@ import IconGrade from '../icons/IconGrade';
 import './GameHud.scss'
 
 class GameHud extends Component {
-    state = {
-        scored: false
-    }
-
     render() {
         return (
             <div className={'game-hud '
                 + this.props.type.split(':').join('')
                 + (this.props.hudCorrectAnswers > 0 ?
-                    ' gain-' + (this.props.hudCorrectAnswers % 2 ? 'even' : 'odd')
+                    ' gain-' + (this.props.hudCorrectAnswers % 2 === 0 ? 'even' : 'odd')
                     :''
                 )
             }>
