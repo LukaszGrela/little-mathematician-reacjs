5941a55e021b8d374446b384d597fd2efdf565c2
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-03-04 00:18:47 +0000

    gameReducer test suite with gamelogic mock.

--- src/tests/reducers/gameReducer.test.js
+++ src/tests/reducers/gameReducer.test.js
@@ -2,6 +2,8 @@ import gameReducer from "../../reducers/gameReducer";
 import { quitGame, gameOver, newGame } from "../../actions/mathGameActions";
 import { GAME_ADDITION } from "../../gameTypes";
 import { generateGameObject } from "../../gamelogic/gamelogic";
+jest.mock('../../gamelogic/gamelogic');
+
 const defaultState = {
     history: [],
     currentGame: null,
@@ -17,9 +19,9 @@ test('Should set currentGame to null for QUIT_GAME action', () => {
     expect(state.currentGame).toBeNull();
 });
 
-test('should add currentGame to history and set currentGame to null for GAME_OVER action', ()=>{
+test('should add currentGame to history and set currentGame to null for GAME_OVER action', () => {
     const currentGame = {
-        game:'my game'
+        game: 'my game'
     };
     const state = gameReducer({
         ...defaultState,
@@ -33,18 +35,35 @@ test('should add currentGame to history and set currentGame to null for GAME_OVE
     expect(state.history[0]).toEqual(currentGame);
 });
 
+test('should add currentGame to history keep history within historyLengthCap limit for GAME_OVER action', () => {
+    const currentGame = {
+        game: 'my game'
+    };
+    const state = gameReducer({
+        ...defaultState,
+        history: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        currentGame
+    }, gameOver());
+
+    expect(state.currentGame).toBeNull();
+
+    expect(state.history.length).toBe(defaultState.historyLengthCap);
+
+    expect(state.history[0]).toEqual(currentGame);
+});
+
 test('should set currentGame to be a new game object', () => {
     const config = {
-        type:GAME_ADDITION,
+        type: GAME_ADDITION,
         general: {
             questionCount: 10,
-            from:0,
-            to:10
+            from: 0,
+            to: 10
         }
     };
 
     const state = gameReducer(defaultState, newGame(config));
-    
+    //
     expect(state.currentGame).toEqual(generateGameObject(config));
 
 });
