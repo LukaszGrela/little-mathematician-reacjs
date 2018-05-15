4b6b70080b02bba9ecaa6a0b5144d48069d9da55
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-05-15 16:03:32 +0100

    added spans around score/time

--- src/components/GameHistoryRow.js
+++ src/components/GameHistoryRow.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import GameTypeIcon from './GameTypeIcon';
 
 const GameHistoryRow = ({ type, correctAnswers, duration, questionCount }) => (
-    <li><GameTypeIcon type={type} /><span>{ `${correctAnswers}/${questionCount} correct answers. It took you ${duration} to finish.`}</span></li>
+    <li>
+        <GameTypeIcon type={type} />
+        <span className="score">{`${correctAnswers}/${questionCount} correct answers.`}</span>
+        <span className="duration">{`It took you ${duration} to finish.`}</span>
+    </li>
 );
 
 GameHistoryRow.propTypes = {
