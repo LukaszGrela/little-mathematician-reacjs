5b445b078a56b60b91fdf218582e276e4db1653d
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-06-14 10:39:33 +0100

    Updated description

--- src/pages/About.js
+++ src/pages/About.js
@@ -26,7 +26,7 @@ class About extends Component {
             <div className='about'>
                 <div className='title'>Little Mathematician</div>
                 <div className='content'>
-                    Simple Math game for children made in ReactJS to learn this framework.
+                    Simple Maths game for children made in ReactJS with aim to explore this framework.
                 </div>
                 <div className='git-source'>
                     <GitHubIcon/><a href="https://github.com/LukaszGrela/little-mathematician-reacjs" 
