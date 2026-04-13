e0058a51fe18cbe99da3093104ddd1cecec7fb8c
LukaszGrela <lukasz.grela@gmail.com>
2026-04-13 21:08:39 +0200

    test MathGame

--- src/pages/MathGame.tsx
+++ src/pages/MathGame.tsx
@@ -33,7 +33,7 @@ export const MathGame: FC = () => {
   const type = match?.params.type as TGameType | undefined;
 
   const dispatch = useDispatch<TAppDispatch>();
-  console.log("MathGame", match?.params.type);
+
   const game = useSelector((state: TRootState) => state.game.currentGame);
 
   const hudQuestionCurrent = useSelector(
@@ -43,7 +43,6 @@ export const MathGame: FC = () => {
   const questionCount = useSelector(
     (state: TRootState) => state.game.currentGame?.questionCount ?? 0,
   );
-  console.log("hudQuestionCurrent", hudQuestionCurrent);
 
   const feedbackActionHandler = useCallback(() => {
     dispatch(nextQuestion());
