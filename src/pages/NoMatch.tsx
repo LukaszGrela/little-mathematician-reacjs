5cd54c9ae3af78611f2645078664cc0fab8af7c2
LukaszGrela <lukasz.grela@gmail.com>
2026-04-13 11:43:03 +0200

    Covert to TSX

---
+++ src/pages/NoMatch.tsx
@@ -0,0 +1,37 @@
+/*
+   Copyright 2018 Łukasz 'Severiaan' Grela GrelaDesign
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+import "./NoMatch.scss";
+
+const NoMatch = () => {
+  return (
+    <div className="no-match-404">
+      <div className="title">
+        <span className="digit d1">4</span>
+        <span className="sign"> - </span>
+        <span className="digit d2">0</span>
+        <span className="sign"> = </span>
+        <span className="digit d3">4</span>
+      </div>
+
+      <div className="content">
+        Sorry, we can't find result you were counting on...
+      </div>
+    </div>
+  );
+};
+
+export default NoMatch;
